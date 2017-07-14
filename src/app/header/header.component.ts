cc3711083f5f7e6927c40da94e2ed92245d3cb88
叶金福 <hzyejinfu@corp.netease.com>
2017-07-14 18:09:24 +0800

    no message

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  userName: string = "叶金福";
+}
+
+if (true) {
+  let a = 1;
+}
+
